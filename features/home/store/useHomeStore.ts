8d249fd80217cac9a28ce6c87c34548b2f41e383
michel llatas <cisnerosllatasmichel@gmail.com>
2026-08-10 15:50:11 -0500

    feat(setup): primer commit

--- features/home/store/useHomeStore.ts
+++ features/home/store/useHomeStore.ts
@@ -1,11 +1,11 @@
 import { create } from "zustand";
-import { getHomeSections } from "@/features/home/services/getHomeSections.core.service";
-import type { HomeSectionDto } from "@/features/home/dtos/getHomeSections.dto";
+// import { getHomeSections } from "@/features/home/services/getHomeSections.core.service";
+// import type { HomeSectionDto } from "@/features/home/dtos/getHomeSections.dto";
 import { HttpError } from "@/shared/api/http/httpClient";
 import { notify } from "@/shared/lib/notify";
 
 type HomeState = {
-  sections: HomeSectionDto[];
+  // sections: HomeSectionDto[];
   isLoading: boolean;
   error: string | null;
   fetchSections: () => Promise<void>;
@@ -20,8 +20,10 @@ export const useHomeStore = create<HomeState>((set) => ({
     set({ isLoading: true, error: null });
 
     try {
-      const sections = await getHomeSections();
-      set({ sections, isLoading: false });
+      // const sections = await getHomeSections();
+      // set({ sections, isLoading: false });
+      
+      set({ isLoading: false });
     } catch (err) {
       const message = err instanceof HttpError ? err.message : "No se pudo cargar la informacion del home";
       set({ error: message, isLoading: false });
--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
             "i.pinimg.com",
         ],
     },
+    output: 'standalone',
 };
 
 export default nextConfig;
